14900cb49abcb32042e38f5d521399583805e0c3
justinebarthelme@gmail.com <justinebarthelme@gmail.com>
2023-01-19 13:57:06 +0100

    findActorMovies

--- src/main/scala/scalix/Scalix.scala
+++ src/main/scala/scalix/Scalix.scala
@@ -5,6 +5,8 @@ import org.json4s.*
 import org.json4s.native.JsonMethods.*
 
 import scala.collection.immutable.HashMap
+import scala.language.postfixOps
+import scala.collection.mutable._
 
 object Scalix extends App {
 
@@ -26,14 +28,42 @@ object Scalix extends App {
   val movieCast = movie("cast").asInstanceOf[List[Any]]
   //Get crew
   val movieCrew = movie("crew").asInstanceOf[List[Any]]
-  println(findActorId("Sigourney", "Weaver").get)
+  //println(findActorId("Sigourney", "Weaver").get)
+  //println(findActorMovies(10205))
 
-  println(movieId)
-  println(movieCast)
-  println(movieCrew)
-
+  //TODO dont forget import scalix.Scalix.*
   def findActorId(name: String, surname: String):Option[Int]=
     val x = movieCast.filter(_.asInstanceOf[HashMap[String, Any]].get("name").get.equals(name + " " + surname))
     Some(x(0).asInstanceOf[HashMap[String, Any]].get("id").get.asInstanceOf[BigInt].toInt)
+
+  /**
+   * val json = parse(contents)
+   *
+   * if (json \ "total_results" == JInt(0)) None
+   *
+   * else Option((json \ "results")(0) \ "id" match {
+   * case JInt(id) => id.toInt
+   * case _ => throw new Exception("Error: id is not an integer")
+   * })
+   */
+
+  def findActorMovies(actorId: Int): Set[(Int, String)]=
+    val request = "https://api.themoviedb.org/3/discover/movie?api_key="+api_key+"&with_people="+actorId
+
+    var array = Set[(Int, String)]()
+
+    val newReq = Source.fromURL(request)
+    val newCon = newReq.mkString
+    val res=parse(newCon).extract[Map[String, Any]]
+    val films=res("results").asInstanceOf[List[Any]]
+    films.foreach((e)=>{
+      val lint=e.asInstanceOf[HashMap[String, Any]].get("id").get.asInstanceOf[BigInt].toInt
+      val lstring=e.asInstanceOf[HashMap[String, Any]].get("title").get.asInstanceOf[String]
+      array += (lint,lstring)
+    })
+    array
+
+  //def findMovieDirector(movieId: Int): Option[(Int, String)] if job type == director
+  //def collaboration(actor1: FullName, actor2: FullName): Set[(String, String)]
 }
 
